c9442215d79d22ba1b74a3961741abff6f2ab090
Brendan Lee <brendanjlee5@gmail.com>
2024-05-27 09:47:06 +0900

    deployment

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+require("dotenv").config();
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
 
 // database connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongDB =
-  "mongodb+srv://brendanjlee5:dbpw123123@cluster0.x1ddt44.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const dev_db_url = `mongodb+srv://${process.env.MONGO_USR}:${process.env.MONGO_PW}@cluster0.x1ddt44.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+// Rate limiter
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
 
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(mongDB);
+  await mongoose.connect(mongoDB);
 }
 
 var indexRouter = require("./routes/index");
@@ -32,6 +43,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+app.use(limiter);
 
 app.use("/", catalogRouter);
 app.use("/catalog", catalogRouter);
